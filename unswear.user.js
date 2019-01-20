24e90bee3c576c1a7ac2922f2b2423ea86bf35a1
devTeaa <devtea@protonmail.com>
2019-01-20 21:18:29 +0700

    remove unused script

--- unswear.user.js
+++ unswear.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         devrant-unswear
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  remove bad words from post
 // @author       devTeaa
 // @match        https://devrant.com/*
@@ -15,23 +15,13 @@
   let badWords = ["fuck"];
   let elementToWatch = ["div.rantlist-title-text", "div.rantlist-tags > a", "div.rantlist-title", "h1.rantlist-content"];
 
-  let badRegex = badWords.map(word => new RegExp(word, "gi"));
+  badWords = badWords.map(word => new RegExp(word, "gi"));
 
   ("use strict");
-  function replaceBadWords(el, random) {
-    badRegex.forEach((reg, i) => {
-      if (random || goodWords.length !== badWords) {
-        i = Math.floor(Math.random() * Math.floor(goodWords.length - 1));
-        el.innerHTML = el.innerHTML.replace(reg, goodWords[i]);
-      } else {
-        el.innerHTML = el.innerHTML.replace(reg, goodWords[i]);
-      }
-    });
-  }
 
   elementToWatch.forEach(element => {
     document.querySelectorAll(element).forEach(el => {
-      badRegex.forEach((reg, i) => {
+      badWords.forEach((reg, i) => {
         if (randomMode || goodWords.length !== badWords) {
           i = Math.floor(Math.random() * Math.floor(goodWords.length - 1));
           el.innerHTML = el.innerHTML.replace(reg, goodWords[i]);
