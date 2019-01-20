a8cc85505c882fb8b4fc901df26739bba30f48b2
devTeaa <devtea@protonmail.com>
2019-01-20 21:56:02 +0700

    fix the randomness replacement

--- README.md
+++ README.md
@@ -12,12 +12,15 @@ Download tampermonkey:
 Then install the user-script here:
 https://github.com/devTeaa/devrant-unswear/raw/master/unswear.user.js
 
-You can edit the words and elements starting from line 14
+You can edit the words and elements starting from line 16
 
 ```
-let goodWords = ["❤️"];
+let goodWords = ["❤️", "🎀"];
 let badWords = ["..."];
 let elementToWatch = ["..."];
 ```
 
-![](sc.png)
+Set the randomMode to false on line 14 if you want specific badWords to be replace with their goodWords
+but the goodWords must have the same length as badWords
+
+![](preview.png)
--- unswear.user.js
+++ unswear.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         devrant-unswear
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  remove bad words from post
 // @author       devTeaa
 // @match        https://devrant.com/*
@@ -9,26 +9,33 @@
 // ==/UserScript==
 
 (function() {
-  let randomMode = false;
+  // Change this to false if you want specific badWords to be replace with their goodWords
+  // but the goodWords must have the same lenght as badWords
+  let randomMode = true;
 
   let goodWords = ["❤️", "🎀"];
   let badWords = ["fuck"];
   let elementToWatch = ["div.rantlist-title-text", "div.rantlist-tags > a", "div.rantlist-title", "h1.rantlist-content", "div.related-rant-text"];
 
-  badWords = badWords.map(word => new RegExp(word, "gi"));
+  let badWordsReg = RegExp(badWords.join("|"));
+  badWords = badWords.map(word => new RegExp(word, "i"));
 
   ("use strict");
 
   elementToWatch.forEach(element => {
     document.querySelectorAll(element).forEach(el => {
-      badWords.forEach((reg, i) => {
-        if (randomMode || goodWords.length !== badWords) {
-          i = Math.floor(Math.random() * Math.floor(goodWords.length));
-          el.innerHTML = el.innerHTML.replace(reg, goodWords[i]);
+      while (badWordsReg.test(el.innerHTML)) {
+        if (goodWords.length === badWords.length && !randomMode) {
+          badWords.forEach((reg, i) => {
+            el.innerHTML = el.innerHTML.replace(reg, goodWords[i]);
+          });
         } else {
-          el.innerHTML = el.innerHTML.replace(reg, goodWords[i]);
+          let i = Math.floor(Math.random() * Math.floor(goodWords.length));
+          badWords.forEach(reg => {
+            el.innerHTML = el.innerHTML.replace(reg, goodWords[i]);
+          });
         }
-      });
+      }
     });
   });
 })();
