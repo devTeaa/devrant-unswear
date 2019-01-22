bfecad573895d57b0a6daa32445d2b149836a35b
devFreeTea <devtea@protonmail.com>
2019-01-22 08:02:36 +0700

    fix verb with tense regex

--- unswear.user.js
+++ unswear.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         devrant-unswear
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  Remove bad words from any post
 // @author       devTeaa
 // @match        https://devrant.com/*
@@ -10,31 +10,55 @@
 
 (function() {
   // Change randomMode to false if you want specific badWords to be replaced with their goodWords.
-  // If randomMode's false, goodWords must have the same length as badWords. 
+  // If randomMode's false, goodWords must have the same length as badWords.
   let randomMode = true;
 
-  let goodWords = ["❤️", "🎀", "hug"];
-  let badWords = ["fuck(\\w+)?", "motherfuck(\\w+)?", "shit(\\w+)?", "bitch(\\w+)?"];
-  
+  // Word to watch/find
+  let badWords = ["fuck", "motherfuck", "shit", "bitch"];
+  // and will be replaced with
+  let goodWords = ["❤️", "🎀", "🤬", "hug", "duck"];
+
+  // List of element to watch
   let elementToWatch = ["div.rantlist-title-text", "div.rantlist-tags > a", "div.rantlist-title", "h1.rantlist-content", "div.related-rant-text"];
 
+  // Create badWords and verb with tenses regex
   let badWordsReg = RegExp(badWords.join("|"));
-  badWords = badWords.map(word => new RegExp(word, "i"));
+  let badWordsTenseReg = RegExp(`(${badWords.join("|")})+(ing|ed)+`);
 
+  badWords = badWords.map(word => new RegExp(word, "i"));
   ("use strict");
 
+  // Safe while loop break
+  let breakLoop = 0;
   elementToWatch.forEach(element => {
     document.querySelectorAll(element).forEach(el => {
       while (badWordsReg.test(el.innerHTML)) {
+        // Replace badWords witht their goodWords repectively
         if (goodWords.length === badWords.length && !randomMode) {
           badWords.forEach((reg, i) => {
             el.innerHTML = el.innerHTML.replace(reg, goodWords[i]);
           });
         } else {
-          let i = Math.floor(Math.random() * Math.floor(goodWords.length));
-          badWords.forEach(reg => {
-            el.innerHTML = el.innerHTML.replace(reg, goodWords[i]);
-          });
+          // Find verb with tenses, and replaced with goodWords that is less than 3 letters only
+          let i = Math.floor(Math.random() * Math.floor(goodWords.filter(x => x.length < 3).length));
+          let detectedWord = el.innerHTML.match(badWordsTenseReg);
+          let replacementWord = goodWords.filter(x => x.length < 3)[i];
+
+          if (detectedWord === null) {
+            // If no verb with tenses left then replace randomly
+            i = Math.floor(Math.random() * Math.floor(goodWords.length));
+            detectedWord = el.innerHTML.match(badWordsReg);
+            replacementWord = goodWords[i];
+          }
+
+          el.innerHTML = el.innerHTML.replace(detectedWord[0], replacementWord);
+        }
+
+        // Safe while loop break
+        if (breakLoop < 1000) {
+          breakLoop++;
+        } else {
+          break;
         }
       }
     });
